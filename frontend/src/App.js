45e0b7cbf1a428ea0cf756a31a79d8c18c26a1d2
Daniela <danielacardona2203@gmail.com>
2022-12-07 11:35:57 +0100

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Tarot-warehouse</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
