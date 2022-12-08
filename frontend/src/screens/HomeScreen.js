eaf8c606b9177e025b2b5652b5e32441564ab7a9
Daniela <danielacardona2203@gmail.com>
2022-12-08 13:24:29 +0100

    installed react-helmet, big update in product detai

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,6 +4,7 @@ import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
 
 // import data from "../data";
 
@@ -26,6 +27,7 @@ function HomeScreen() {
     loading: true,
     error: '',
   });
+  
   // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
@@ -43,6 +45,11 @@ function HomeScreen() {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>
+          Tarot-warehouse
+        </title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
