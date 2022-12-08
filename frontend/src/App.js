92f9942a070c3261751a70e4ccde980d30ba6ebe
Daniela <danielacardona2203@gmail.com>
2022-12-08 16:52:28 +0100

    create a React Context to save shopping Cart items.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,52 @@
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import {LinkContainer} from 'react-router-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from 'react';
+import { Store } from './Store';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <BrowserRouter>
-    <div className='d-flex flex-column site-container'>
-      <header>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <LinkContainer to="/">
-            <Navbar.Brand>Tarot-warehouse</Navbar.Brand>
-            </LinkContainer>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Tarot-warehouse</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-5">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
           </Container>
-        </Navbar>
-      </header>
-      <main>
-      <Container className='mt-5'>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>  
-        </Container>     
-      </main>
-      <footer>
-        <div className='text-center'>
-          All rigths reserved
-        </div>
-      </footer>
-    </div>
+        </main>
+        <footer>
+          <div className="text-center">All rigths reserved</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
