805f3f1b0fac0d3714ae8f47a4e1288ea791ebb4
Daniela <danielacardona2203@gmail.com>
2022-12-09 12:27:50 +0100

    cart icon added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,11 +22,11 @@ function App() {
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
-                <Navbar.Brand>Tarot-warehouse</Navbar.Brand>
+                <Navbar.Brand> 🧙🏽 Tarot-warehouse</Navbar.Brand>
               </LinkContainer>
               <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
-                  Cart
+                <i className="fas fa-shopping-cart"></i>
                   {cart.cartItems.length > 0 && (
                     <Badge pill bg="danger">
                       {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
